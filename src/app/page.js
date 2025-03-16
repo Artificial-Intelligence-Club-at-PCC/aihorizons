46bdfcf1feb20b9650889839fb43ae351a18cc59
EvanJayChou <evan.chou@live.com>
2025-03-16 11:12:55 -0700

    Project Start
    Initiated Next.js app, typed in some HTML code into the JS file that is tentative and just for display purposes.

---
+++ src/app/page.js
@@ -0,0 +1,177 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <html>
+      <body>
+        <header>
+
+        </header>
+
+        <main>
+          <section id='main'>
+            <div>
+              <h1>AI Horizons Convention</h1>
+              <h2>April 30th, 2025: 11 AM to 6 PM</h2>
+              <h3>Creveling Lounge (CC 201), Pasadena City College</h3>
+            </div>
+            <h4>Presented by Artificial Intelligence Club at PCC</h4>
+          </section>
+
+          <section id='about'>
+
+          </section>
+
+          <section id='itinerary'>
+            <div>
+              <h1>11 AM - 1 PM: Opening Remarks, Exhibits, and Lunch</h1>
+                <p></p>
+            </div>
+
+            <div>
+              <h1>1 - 2 PM: The Future of AI in Enterprises and Society</h1>
+                <div>
+                  <h1>David Chou</h1>
+                    <h2>Director of Cloud Developer and Platform Strategy at Microsoft</h2>
+                    <p></p>
+                </div>
+            </div>
+
+            <div>
+              <h1>2:15 - 3:00 PM: AI in Robotics Systems and Human Collaboration</h1>
+                <div>
+                  <h1>Rob Royce</h1>
+                    <h2>Data Scientist and AI Advisor at NASA Jet Propulsion Laboratory</h2>
+                    <p></p>
+                </div>
+            </div>
+
+            <div>
+              <h1>3:15 - 4:15 PM: The Impact of AI in Industry and Education</h1>
+                <div>
+                  <h1>Rob Royce</h1>
+                    <h2>Data Scientist and AI Advisor at NASA Jet Propulsion Laboratory</h2>
+                    <p></p>
+                </div>
+                <div>
+                  <h1>Foster Brereton</h1>
+                    <h2>Principal Scientist at Adobe</h2>
+                    <p></p>
+                </div>
+                <div>
+                  <h1>Jensen Huang</h1>
+                    <h2>CEO of NVIDIA</h2>
+                    <p></p>
+                </div>
+                <div>
+                  <h1>Dr. Thomas Hou</h1>
+                    <h2>Charles Lee Powell Professor of Applied Computational Mathematics at Caltech</h2>
+                    <p></p>
+                </div>
+            </div>
+
+            <div>
+              <h1>4:30 - 5:30 PM: Accelerate your Career, Research, and Internships</h1>
+                <div>
+                  <h1>Xianmei Li</h1>
+                    <h2>Robotics Systems Engineer at NASA Jet Propulsion Laboratory</h2>
+                    <p></p>
+                </div>
+                <div>
+                  <h1>Idk</h1>
+                    <h2>Student at UCLA</h2>
+                </div>
+                <div>
+                  <h1>Idk</h1>
+                    <h2>Student at Caltech</h2>
+                </div>
+                <div>
+                  <h1>Chandler Desforges</h1>
+                    <h2>Computer Science Student at Pasadena City College</h2>
+                </div>
+            </div>
+
+            <div>
+              <h1>5:30 - 6:00 PM: Closing Remarks and Networking</h1>
+                <p></p>
+            </div>
+
+          </section>
+
+          <section id='sections&collabs'>
+            <div>
+              <h1>Sponsored By:</h1>
+              {/*Images, not text*/}
+            </div>
+            <div>
+              <h1>In Collaboration With:</h1>
+              {/*Images, not text*/}
+            </div>
+          </section>
+
+          <section id='organizers'>
+            <section id='aiclub'>
+              <h1>AI Club Organizers</h1>
+              <div>
+                <h1>Evan Chou</h1>
+                  <h2>President</h2>
+              </div>
+              <div>
+                <h1>Shin Aung</h1>
+                  <h2>Vice President</h2>
+              </div>
+              <div>
+                <h1>Akki Singh</h1>
+                  <h2>Secretary</h2>
+              </div>
+              <div>
+                <h1>Jordan Marcelo</h1>
+                  <h2>Treasurer</h2>
+              </div>
+              <div>
+                <h1>Vivian Nguyen</h1>
+                  <h2>Treasurer</h2>
+              </div>
+              <div>
+                <h1>Nyan Lynn Phyoe Zaw</h1>
+                  <h2>Events Coordinator</h2>
+              </div>
+              <div>
+                <h1>Jian Wang</h1>
+                  <h2>Social Media Manager</h2>
+              </div>
+              <div>
+                <h1>Tuan Dien Tran</h1>
+                  <h2>ICC Representative</h2>
+              </div>
+              <div>
+                <h1>David Alcala</h1>
+                  <h2>Public Relations</h2>
+              </div>
+              <div>
+                <h1>Timothy Delange</h1>
+                  <h2>Project Manager</h2>
+              </div>
+            </section>
+            <section id='faculty'>
+              <h1>Faculty Advisors</h1>
+              <div>
+                <h1>Dr. Jamal Ashraf</h1>
+                  <h2>AI Club Advisor</h2>
+              </div>
+              <div>
+                <h1>Dr. Yu-Chang Chang-Hou</h1>
+                  <h2>AI Horizons Convention Advisor</h2>
+              </div>
+            </section>
+          </section>
+
+        </main>
+
+        <footer>
+          <p>&copy; 2025 AI Horizons Convention. All rights reserved.</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
